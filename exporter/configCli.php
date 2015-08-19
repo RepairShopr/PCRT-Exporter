31bb8ba29bb36c3222d38cc1c64717775317fceb
eduardBoikov <eduard.boikov@gmail.com>
2015-08-19 11:57:45 +0300

    CLI Expoter
    Exporter was rewritten for CLI.
    
    Database: mysql_* functions are deprecated.
    For customers, tickets and invoices - now using PDO; Cusomter assets
    uses original code (mysql_*).
    
    Invoices and Cusomer assets (original codes as wel) - don't work
    properly, possibly due to api changes.

---
+++ exporter/configCli.php
@@ -0,0 +1,10 @@
+<?php
+          define('DATABASE_HOST', 'localhost');
+          define('DATABASE_NAME', '');
+          define('DATABASE_USERNAME', '');
+          define('DATABASE_PASSWORD', '');
+          define('RS_API_KEY', '');
+          define('RS_SUBDOMAIN', '');
+          define('BASE_URL',"https://".RS_SUBDOMAIN.".repairshopr.com");
+          define('API_VERSION',"/api/v1");
+        ?>
